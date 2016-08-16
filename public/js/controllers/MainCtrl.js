849aae2fbdfd7252cbc99653c4ec529842f3fc06
butterpunk <blakebutterworth@gmail.com>
2016-08-15 20:47:26 -0500

    skeleton app

---
+++ public/js/controllers/MainCtrl.js
@@ -0,0 +1,37 @@
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
+
+	$scope.dares=[{
+		place: 'St. Charles, Uptown',
+		points: '75',
+		description: 'take a walk down st. charles at night with a bottle of wine.'
+	},{
+		place: 'Frenchman Street',
+		points: '100',
+		description: 'Make out with 3 people in one night on frenchman St.'
+	},{
+		place: 'Bacchanal',
+		points: '100',
+		description: 'Smoke a joint in Bacchanal'		
+	},{
+		place: 'Apple Barrel',
+		points: '100',
+		description: 'Take A Shot with Morgan, the bartender'			
+	},{
+		place: 'The Country Club',
+		points: '100',
+		description: 'Go to the drag brunch and tip the queens'			
+	},{
+		place: 'Chickie Wah Wah',
+		points: '100',
+		description: 'See Alexis and the Samauri monday residential'			
+	},{
+		place: 'Heavens Gate',
+		points: '100',
+		description: 'Go to a house show and bring dorritos'			
+	},
+
+
+
+	]
+
+});
