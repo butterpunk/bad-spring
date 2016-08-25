885f922038d32ec3d3a192b0c57418696a4b990b
unknown <pbordeaux@BTC-DT05.belltec.com>
2016-08-25 13:22:27 -0500

    google maps API angular directive integration

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,14 +1,27 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
-	document.addEventListener('DOMContentLoaded', function() {
-	   // your code here
-	      var map;
-	      function initMap() {
-	        map = new google.maps.Map(document.getElementById('map'), {
-	          center: {lat: -34.397, lng: 150.644},
-	          zoom: 8
-	        });
-	      }
-	}, false); 
+angular.module('MainCtrl', ['uiGmapgoogle-maps'])
+.config(
+	['$provide', 'nemSimpleLoggerProvider', 'uiGmapGoogleMapApiProvider', function($provide, nemSimpleLoggerProvider, uiGmapGoogleMapApiProvider) {
+		$provide.decorator.apply(null, nemSimpleLoggerProvider.decorator);
+		   //this loads gmaps api 
+		   uiGmapGoogleMapApiProvider.configure({
+	        key: 'AIzaSyCCFTekcXn3C9VY94-eqsYjRuecuSiRhHc', //I got a different API toke
+	        libraries: 'weather,geometry,visualization'
+	    });
+}])
+.controller('MainController', function($scope, $log, uiGmapGoogleMapApi) {
+	$log.currentLevel = $log.LEVELS.debug;
+	
+	//example using bachannal's coords
+	//in style.css
+	//***  .angular-google-map-container { height: 400px; } ***
+	//this controls the svg's size, if you don't set it, it is 0
+	//learned that the hard way.
+  $scope.map = {center: {latitude: 29.960126, longitude: -90.033251 }, zoom: 14 };
+  $scope.options = {scrollwheel: false};
+
+  uiGmapGoogleMapApi.then(function(maps) {
+  	console.log(maps);
+  });
 
 	$scope.date = 'August 19, 2016';
 	$scope.dares=[{
