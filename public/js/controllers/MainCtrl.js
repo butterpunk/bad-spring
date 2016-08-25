5b2be11c873fd952b5e8779131450c803c7e3ded
butterpunk <blakebutterworth@gmail.com>
2016-08-25 18:00:55 -0500

    try deploying again AGAIN

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -26,7 +26,120 @@ angular.module('MainCtrl', ['uiGmapgoogle-maps'])
   	//      https://developers.google.com/maps/documentation/javascript/reference#MapTypeStyle
   	//those are the three props for the styles array and just go nuts from there
   	styles: [
-					    {
+			     {
+			        "featureType": "road",
+			        "elementType": "labels",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "poi",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "administrative",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road",
+			        "elementType": "geometry.fill",
+			        "stylers": [
+			            {
+			                "color": "#000000"
+			            },
+			            {
+			                "weight": 1
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "road",
+			        "elementType": "geometry.stroke",
+			        "stylers": [
+			            {
+			                "color": "#000000"
+			            },
+			            {
+			                "weight": 0.8
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "landscape",
+			        "stylers": [
+			            {
+			                "color": "#ffffff"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "water",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "featureType": "transit",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels",
+			        "stylers": [
+			            {
+			                "visibility": "off"
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels.text",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels.text.stroke",
+			        "stylers": [
+			            {
+			                "color": "#ffffff"
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels.text.fill",
+			        "stylers": [
+			            {
+			                "color": "#000000"
+			            }
+			        ]
+			    },
+			    {
+			        "elementType": "labels.icon",
+			        "stylers": [
+			            {
+			                "visibility": "on"
+			            }
+			        ]
+			    } 				
+				/*{
 					        "featureType": "all",
 					        "elementType": "geometry",
 					        "stylers": [
@@ -139,7 +252,7 @@ angular.module('MainCtrl', ['uiGmapgoogle-maps'])
 					                "lightness": -20
 					            }
 					        ]
-					    }
+					    } */
 					]
   	};
 
