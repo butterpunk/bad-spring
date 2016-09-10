028b19ce2afb2d50dd1be6cd164c29c9d7ec05aa
Blake butterworth <blakebutterworth@blakes-air.attlocal.net>
2016-09-10 14:17:40 -0500

    filled in lat and long for places

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -140,31 +140,46 @@ angular.module('MainCtrl', ['uiGmapgoogle-maps'])
 	$scope.dares=[{
 		place: 'St. Charles, Uptown',
 		points: 75,
-		description: 'take a walk down st. charles at night with a bottle of wine.'
+		description: 'take a walk down st. charles at night with a bottle of wine.',
+		lat: 29.9279,
+		long: -90.1279
 	},{
 		place: 'Frenchman Street',
 		points: 100,
-		description: 'Make out with 3 people in one night on frenchman St.'
+		description: 'Make out with 3 people in one night on frenchman St.',
+		lat: 29.951065,
+		long: -90.071533
+		
 	},{
 		place: 'Bacchanal',
 		points: 100,
-		description: 'Smoke a joint in Bacchanal'		
+		description: 'Smoke a joint in Bacchanal',
+		lat: 29.960126,
+		long: -90.033251		
 	},{
 		place: 'Apple Barrel',
 		points: 100,
-		description: 'Take A Shot with Morgan, the bartender'			
+		description: 'Take A Shot with Morgan, the bartender',
+		lat: 29.9638,
+		long: -90.0577					
 	},{
 		place: 'The Country Club',
 		points: 100,
-		description: 'Go to the drag brunch and tip the queens'			
+		description: 'Go to the drag brunch and tip the queens',
+		lat: 29.962687,
+		long: -90.044142					
 	},{
 		place: 'Chickie Wah Wah',
 		points: 100,
-		description: 'See Alexis and the Samauri monday residential'			
+		description: 'See Alexis and the Samauri monday residential',
+		lat: 29.9667653,
+		long: -90.0917809					
 	},{
 		place: 'Heavens Gate',
 		points: 100,
-		description: 'Go to a house show and bring dorritos'			
+		description: 'Go to a house show and bring dorritos',
+		lat: 29.948919,
+		long: -90.117925					
 	},]
 
 	$scope.currentPage = 0;
