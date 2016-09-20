46056ee57fbd275fa012f63f37623983b8b57c10
Blake Butterworth <blakebutterworth@gmail.com>
2016-09-20 08:02:05 -0500

    popup scroll fix

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -281,6 +281,6 @@ jQuery(document).mouseup(function (e)
 	$scope.lightning = function(arg){
 		$scope.popupChallenge = $scope.dares[arg].description;
 		jQuery('.popup').removeClass('hide');
-		document.body.style.overflow = "hidden";
+		jQuery('body').css('overflow','hidden')
 	}	
 });
