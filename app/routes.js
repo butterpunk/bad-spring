04da0f704d1fb2ce64b80f10ab821d797b9d84bf
Blake Butterworth <blakebutterworth@gmail.com>
2016-09-29 13:16:09 -0500

    got initial api node api calls working for challenges

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 // grab the nerd model we just created
 var Nerd = require('./models/nerd');
+var Challenge = require('./models/challenge');
 
 module.exports = function(app) {
 
@@ -11,14 +12,46 @@ module.exports = function(app) {
         // use mongoose to get all nerds in the database
         Nerd.find(function(err, nerds) {
 
-            // if there is an error retrieving, send the error. 
-                            // nothing after res.send(err) will execute
+            console.log('here');
+
             if (err)
                 res.send(err);
 
             res.json(nerds); // return all nerds in JSON format
         });
-    });	
+    });
+
+    app.post('/api/challenges',function(req,res){      
+        var challenge = new Challenge();
+        
+        challenge.place = req.body.place;
+        challenge.points = req.body.points;
+        challenge.description = req.body.description;
+        challenge.lat = req.body.lat;
+        challenge.long = req.body.long;
+
+        challenge.save(function(err){
+            if (err)
+                res.send(err);
+
+            res.json({message: 'Challenge Created!'})
+        });
+
+    });
+
+    app.get('/api/challenges', function(req, res) {
+        // use mongoose to get all nerds in the database
+        Challenge.find(function(err, nerds) {
+
+            console.log('here');
+
+            if (err)
+                res.send(err);
+
+            res.json(nerds); // return all nerds in JSON format
+        });
+    }); 
+
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
