e61337f1a6982afbad515333ddb13493d394373a
Blake Butterworth <blakebutterworth@gmail.com>
2016-10-03 15:51:37 -0500

    added initial logic for signup - p sure its working

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 var Nerd = require('./models/Nerd.js');
 var Challenge = require('./models/Challenge.js');
 
-module.exports = function(app) {
+module.exports = function(app,passport) {
 
 	// server routes ===========================================================
 	// handle things like api calls
@@ -102,7 +102,27 @@ module.exports = function(app) {
             res.json(nerds); // return all nerds in JSON format
         });
     }); 
+ 
 
+    app.get('/user/login', function(req,res){
+
+    });
+    /*
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/geek', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));*/
+    app.post('/signup', passport.authenticate('local-signup'));    
+
+    /*
+    app.get('/user/signup',function(req,res){
+
+    });
+
+    app.get('/user/login',function(req,res){
+
+    });*/
 	// frontend routes =========================================================
 	// route to handle all angular requests
 	app.get('*', function(req, res) {
@@ -110,3 +130,13 @@ module.exports = function(app) {
 	});
 
 };
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
