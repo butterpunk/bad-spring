8b372805687ebb3b06dc6c6a291044caa05ebbec
Blake Butterworth <blakebutterworth@gmail.com>
2016-10-04 15:53:27 -0500

    added passport file

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	url : 'mongodb://127.0.0.1:27017'
-	//url: 'mongodb://devHeroku:e2UzKVjprRJ2EcpK@ds049436.mlab.com:49436/heroku_l3mktg5q'
+	//url : 'mongodb://127.0.0.1:27017'
+	url: 'mongodb://devHeroku:e2UzKVjprRJ2EcpK@ds049436.mlab.com:49436/heroku_l3mktg5q'
 }
