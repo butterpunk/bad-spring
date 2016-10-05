c2897c8c1d40349858eaea6b66a555b67aaa99d4
Blake Butterworth <blakebutterworth@gmail.com>
2016-10-05 11:20:46 -0500

    maybe fixing mapp issue?

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -51,10 +51,6 @@ angular.module('MainCtrl', ['uiGmapgoogle-maps'])
 		 uiGmapGoogleMapApi.then(function(marker) {
 		  	console.log(marker);
 		  });
-		$scope.map = {
-	  					center: {latitude: 29.960126, longitude: -90.033251 }, 
-	  					zoom: 13       
-	      			};
 
     });
 
@@ -178,7 +174,10 @@ $scope.exitlogin = function(){
 			});			
 		}
 	}
-
+		$scope.map = {
+	  					center: {latitude: 29.960126, longitude: -90.033251 }, 
+	  					zoom: 13       
+	      			};
   // $scope.marker = {coords: {latitude: 29.960126, longitude: -90.033251 }, id: 1, options: {icon: '../assets/gold_bolt.png'} };
   	$scope.options = {
 	  	scrollwheel: false, 
